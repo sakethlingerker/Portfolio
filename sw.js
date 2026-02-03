1f7ca5aab02cf4ded8c048b4d2a6c80fd98a58cd
sakethlingerker <135976032+sakethlingerker@users.noreply.github.com>
2026-02-03 17:48:25 +0000

    Deploying to gh-pages from @ sakethlingerker/Portfolio@cde38325543125510c159b25ae85f3c1fdc60fdf 🚀

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "saketh-portfolio-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./assets/ls-logo.jpg",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "https://unpkg.com/aos@2.3.1/dist/aos.css",
+  "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap",
+];
+
+// Install Event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }),
+  );
+});
+
+// Activate Event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+// Fetch Event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response if found
+      if (response) {
+        return response;
+      }
+      // Otherwise fetch from network
+      return fetch(event.request);
+    }),
+  );
+});
